e8fe2d6d284252c0aa0d0d0e7a96d5e97d0b3b60
Malith-ra <IT18097634@my.sliit.lk>
2021-03-18 21:24:02 +0530

    commit

---
+++ app/src/components/Banner.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Button, Colors } from "react-native-paper";
+import ConstantColors from "../constants/ConstantColors";
+
+export class Banner extends Component {
+  render() {
+    return (
+      // <View style={{flex: 1, backgroundColor: "green", width: "100%"}}>
+        <Button
+          mode="contained"
+          color={ConstantColors.tintColor}
+          style={styles.sloganButton}
+          onPress={() => console.log("Pressed")}
+        >
+          <Text style={{ color: "white", fontSize: 20 }}>DHANGAADI</Text>
+        </Button>
+      // </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  sloganButton: {
+    flex: 1,
+    justifyContent: "center",
+    // width: "150%",
+    // height: "40%",
+    // borderRadius: 40,
+    borderWidth: 1,
+    borderColor: ConstantColors.tintColor,
+    // elevation: 20,
+    // marginTop: 20
+  },
+});
+
+export default Banner;
---
+++ client/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./nprogress.css";
+
+const Nav = () => (
+  <div>
+    <h1
+    style={{ textAlign: "center" }}
+    >Welcome to Easy Bus Booking System</h1>
+  </div>
+);
+
+export default Nav;
